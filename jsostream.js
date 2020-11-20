56cc61483b037f63f7ae338734bf14b7ad7d41d3
jonathanmarp <60574415+jonathanmarp@users.noreply.github.com>
2020-11-20 08:22:08 +0700

    Create and Provide For Jsostream

--- jsostream.js
+++ jsostream.js
@@ -1,1 +1,266 @@
-const _0x3a80=['fstream','readFile','getEnum','http','_0x01','writeFile','Please\x20Give\x20Name\x20Of\x20Variable','varName','randomString','log','openFile','_0b0wifniqfneoafboa','toArrText','appendFile','toChar','_0x02','shift','print','float','floor','int','push','Name\x20Variable\x20Undefined','open','randomNumber','trim','runSave','jsostream','length','split','var','sort','random','pushData','nameFile','getData','splice','importFunctions','call','qwertyuiopasdfghjklzxcvbnm','DeleteData','varFill','argv','changeFirstNumber'];(function(_0x2231ec,_0x382077){const _0x3a80e6=function(_0x114997){while(--_0x114997){_0x2231ec['push'](_0x2231ec['shift']());}};_0x3a80e6(++_0x382077);}(_0x3a80,0x9e));const _0x1149=function(_0x2231ec,_0x382077){_0x2231ec=_0x2231ec-0x181;let _0x3a80e6=_0x3a80[_0x2231ec];return _0x3a80e6;};const _0x3816a8=_0x1149,process=require('process'),http=require(_0x3816a8(0x196)),fs=require('fs');exports[_0x3816a8(0x194)]=function(_0x4c0ae0,_0x3f973e){const _0x5c68a4=_0x3816a8;fs[_0x5c68a4(0x194)](_0x4c0ae0,function(_0x491a26,_0x66ecf0){_0x3f973e(_0x66ecf0,_0x491a26);});};class Fstream{constructor(_0x4608b4){const _0x4997a2=_0x3816a8;this[_0x4997a2(0x189)]=_0x4608b4;}[_0x3816a8(0x198)](_0x444032,_0x57dfb9){const _0x54401b=_0x3816a8;fs[_0x54401b(0x198)](this[_0x54401b(0x189)],_0x444032,_0x57dfb9);}['openFile'](_0x35bb0,_0x416f94){const _0x3d79bd=_0x3816a8;fs['open'](this[_0x3d79bd(0x189)],_0x35bb0,_0x416f94);}[_0x3816a8(0x1a0)](_0x19293f,_0xcfebdc){const _0x1785f7=_0x3816a8;fs[_0x1785f7(0x1a0)](this[_0x1785f7(0x189)],_0x19293f,_0xcfebdc);}}exports[_0x3816a8(0x193)]=_0x24a1f6=>new Fstream(_0x24a1f6),exports[_0x3816a8(0x1a0)]=function(_0x47d0df,_0xc0fc0b,_0xaf9dfe){const _0x2f5bc9=_0x3816a8;fs[_0x2f5bc9(0x1a0)](_0x47d0df,_0xc0fc0b,_0xaf9dfe);},exports[_0x3816a8(0x19d)]=function(_0x37fe2f,_0x159e4d,_0x1e06f8){const _0x51897f=_0x3816a8;fs[_0x51897f(0x1aa)](_0x37fe2f,_0x159e4d,_0x1e06f8);},exports['writeFile']=function(_0x58487e,_0xb59d3c,_0x35f15c){fs['writeFile'](_0x58487e,_0xb59d3c,_0x35f15c);};let nameFunctions=[],allFunctions=[];exports['functions']=function(_0x5b2830,_0x3732f9){const _0xfbd3f2=_0x3816a8;nameFunctions[_0xfbd3f2(0x1a8)](_0x5b2830),allFunctions[_0xfbd3f2(0x1a8)](_0x3732f9);},exports[_0x3816a8(0x18d)]=function(_0x213d1c,..._0x275ce1){const _0x2b7338=_0x3816a8;for(let _0xb8074d=0x0;_0xb8074d<nameFunctions[_0x2b7338(0x183)];_0xb8074d++){if(nameFunctions[_0xb8074d]==_0x213d1c)return allFunctions[_0xb8074d](_0x275ce1);}throw new Error('Error\x20Not\x20Get\x20Functions');return 0x0;},exports[_0x3816a8(0x18c)]=function(_0x111166){const _0xa7f61a=_0x3816a8;for(let _0x15ac6f=0x0;_0x15ac6f<nameFunctions[_0xa7f61a(0x183)];_0x15ac6f++){if(nameFunctions[_0x15ac6f]==_0x111166)return allFunctions[_0x15ac6f];}throw new Error('Error\x20Not\x20Get\x20Functions');return 0x0;},exports['deleteFunctions']=function(_0x3f0c8b){const _0x4aeed1=_0x3816a8;let _0x5ca636;for(let _0x1d6932=0x0;_0x1d6932<nameFunctions[_0x4aeed1(0x183)];_0x1d6932++){if(nameFunctions[_0x1d6932]==_0x3f0c8b){if(_0x1d6932==0x0)return nameFunctions[_0x4aeed1(0x1a3)](),allFunctions['shift'](),0x0;else _0x5ca636=_0x1d6932;break;}}nameFunctions[_0x4aeed1(0x18b)](_0x5ca636,_0x5ca636),allFunctions[_0x4aeed1(0x18b)](_0x5ca636,_0x5ca636);},exports[_0x3816a8(0x1ab)]=()=>Math[_0x3816a8(0x187)](),exports[_0x3816a8(0x19b)]=function(_0x404dcb=0x0){const _0x4f7531=_0x3816a8;let _0x237646=_0x4f7531(0x18e),_0x46899d='';_0x237646=_0x237646[_0x4f7531(0x184)]('');_0x404dcb==0x0&&(_0x404dcb=_0x237646[_0x4f7531(0x183)]);for(let _0x4a5870=0x0;_0x4a5870<_0x404dcb;_0x4a5870++){_0x46899d+=_0x237646[Math[_0x4f7531(0x1a6)](Math[_0x4f7531(0x187)]()*_0x237646[_0x4f7531(0x183)])];}return _0x46899d;};class enums{constructor(_0x5de2b0){const _0x15853a=_0x3816a8;this[_0x15853a(0x197)]=[],this[_0x15853a(0x1a2)]=[0x0];for(let _0x22e385=0x0;_0x22e385<_0x5de2b0[_0x15853a(0x183)];_0x22e385++){this[_0x15853a(0x197)]['push'](_0x5de2b0[_0x22e385]),this[_0x15853a(0x1a2)][_0x15853a(0x1a8)](_0x22e385+0x1);}}[_0x3816a8(0x192)](_0x5659b6){const _0x205807=_0x3816a8;for(let _0x2eaeab=0x0;_0x2eaeab<this[_0x205807(0x197)][_0x205807(0x183)];_0x2eaeab++){this[_0x205807(0x1a2)][_0x2eaeab]=_0x2eaeab+_0x5659b6;}}[_0x3816a8(0x195)](_0x3c2c67){const _0x159352=_0x3816a8;for(let _0x2a5d5c=0x0;_0x2a5d5c<this[_0x159352(0x197)][_0x159352(0x183)];_0x2a5d5c++){if(this['_0x01'][_0x2a5d5c]==_0x3c2c67)return this[_0x159352(0x1a2)][_0x2a5d5c];}return null;}}exports[_0x3816a8(0x1a1)]=_0x5d2761=>_0x5d2761[_0x3816a8(0x184)](''),exports[_0x3816a8(0x19f)]=_0x56e286=>_0x56e286[_0x3816a8(0x184)]('\x20'),exports[_0x3816a8(0x1a7)]=_0x282e9f=>parseInt(_0x282e9f),exports[_0x3816a8(0x1a5)]=_0x5d6745=>parseFloat(_0x5d6745),exports[_0x3816a8(0x186)]=function(_0x43d255){const _0x695211=_0x3816a8;let _0x10b55b=_0x43d255[_0x695211(0x183)];for(let _0x1cc999=0x0;_0x1cc999<_0x10b55b;_0x1cc999++){for(let _0x4cd118=0x0;_0x4cd118<_0x10b55b;_0x4cd118++){if(_0x43d255[_0x4cd118]>_0x43d255[_0x4cd118+0x1]){let _0x269c88=_0x43d255[_0x4cd118];_0x43d255[_0x4cd118]=_0x43d255[_0x4cd118+0x1],_0x43d255[_0x4cd118+0x1]=_0x269c88;}}}return _0x43d255;},exports['enum']=function(_0x27b0ce){return new enums(_0x27b0ce);},exports[_0x3816a8(0x1a4)]=function(_0x336683,..._0x232fe4){let _0x54db26='';_0x54db26+=_0x336683;for(let _0x2db966=0x0;_0x2db966<_0x232fe4['length'];_0x2db966++){_0x54db26+=_0x232fe4[_0x2db966];}console['log'](_0x54db26);},exports[_0x3816a8(0x182)]=function(_0x42c401){const _0x49b1a7=_0x3816a8;try{_0x42c401(process[_0x49b1a7(0x191)][_0x49b1a7(0x183)],process[_0x49b1a7(0x191)]);}catch(_0x427cec){console[_0x49b1a7(0x19c)](_0x427cec);}},exports[_0x3816a8(0x181)]=function(_0x1ba26e){try{_0x1ba26e();}catch(_0x552b31){return _0x552b31;}};class Variable{constructor(){const _0x254b0b=_0x3816a8;this[_0x254b0b(0x19a)]=[],this[_0x254b0b(0x190)]=[];}['pushData'](_0x23d4ca,_0x539b9e){const _0x15399b=_0x3816a8;this[_0x15399b(0x19a)][_0x15399b(0x1a8)](_0x23d4ca),this[_0x15399b(0x190)][_0x15399b(0x1a8)](_0x539b9e);}[_0x3816a8(0x18a)](_0x429302){const _0x3a3a78=_0x3816a8;for(let _0x218548=0x0;_0x218548<this[_0x3a3a78(0x19a)][_0x3a3a78(0x183)];_0x218548++){if(this[_0x3a3a78(0x19a)][_0x218548]==_0x429302)return this[_0x3a3a78(0x190)][_0x218548];}return _0x3a3a78(0x19e);}['DeleteData'](_0x22470e=null){const _0x5b4e80=_0x3816a8;_0x22470e=_0x22470e[_0x5b4e80(0x1ac)]();if(_0x22470e==null||_0x22470e=='')throw new Error(_0x5b4e80(0x199));let _0x55a1bf;for(let _0x3d83e4=0x0;_0x3d83e4<this[_0x5b4e80(0x19a)][_0x5b4e80(0x183)];_0x3d83e4++){if(this['varName'][_0x3d83e4]==_0x22470e){if(_0x3d83e4==0x0)return this[_0x5b4e80(0x19a)][_0x5b4e80(0x1a3)](),this[_0x5b4e80(0x190)]['shift'](),0x0;else _0x55a1bf=_0x3d83e4;break;}}this['varName'][_0x5b4e80(0x18b)](_0x55a1bf,_0x55a1bf),this[_0x5b4e80(0x190)]['splice'](_0x55a1bf,_0x55a1bf);}}let _0x0a1Var=new Variable();exports[_0x3816a8(0x185)]=function(_0x2a7e05=null,_0x46a8f3){const _0x5e4ec2=_0x3816a8;_0x2a7e05=_0x2a7e05[_0x5e4ec2(0x1ac)]();if(_0x2a7e05==null||_0x2a7e05=='')throw new Error(_0x5e4ec2(0x199));_0x0a1Var[_0x5e4ec2(0x188)](_0x2a7e05,_0x46a8f3);},exports['vEquation']=function(_0x43ee96,..._0x4d722d){const _0x365612=_0x3816a8;let _0x2d7241=0x0;for(let _0x44110e=0x0;_0x44110e<_0x0a1Var[_0x365612(0x19a)][_0x365612(0x183)];_0x44110e++){_0x0a1Var[_0x365612(0x19a)][_0x44110e]==_0x43ee96&&(_0x2d7241=_0x44110e);}for(let _0x1daa33=0x0;_0x1daa33<_0x4d722d[_0x365612(0x183)];_0x1daa33++){for(let _0x467fe7=0x0;_0x467fe7<_0x0a1Var[_0x365612(0x19a)][_0x365612(0x183)];_0x467fe7++){_0x4d722d[_0x1daa33]==_0x0a1Var['varName'][_0x467fe7]&&(_0x0a1Var[_0x365612(0x190)][_0x467fe7]=_0x0a1Var[_0x365612(0x190)][_0x2d7241]);}}},exports['vGet']=function(_0xacc4f8=null){const _0x2e9d0c=_0x3816a8;_0xacc4f8=_0xacc4f8[_0x2e9d0c(0x1ac)]();if(_0xacc4f8==null||_0xacc4f8=='')throw new Error('Please\x20Give\x20Name\x20Of\x20Variable');let _0x9e5a76=_0x0a1Var['getData'](_0xacc4f8);if(_0x9e5a76==_0x2e9d0c(0x19e))throw new Error(_0x2e9d0c(0x1a9));else return _0x9e5a76;},exports['vDelete']=function(_0x129739=null){const _0x7f123b=_0x3816a8;_0x129739=_0x129739[_0x7f123b(0x1ac)]();if(_0x129739==null||_0x129739=='')throw new Error(_0x7f123b(0x199));_0x0a1Var[_0x7f123b(0x18f)](_0x129739);};
+/*******************************************************\
+ *              COPYRIGHT 2020 - 2025                  *
+ *******************************************************
+ *   Have privacy police this made by website          *
+ *   https://jonathanmarpaung.rf.gd/                   *
+ *   this for publish CODE                             *
+ *((((((((((((((((((((((((((C))))))))))))))))))))))))))*
+\*******************************************************/
+
+const process = require('process');
+const http = require('http');
+const fs = require('fs');
+
+exports.readFile = function(nameFile, callback) {
+    fs.readFile(nameFile, function(err, data) {
+        callback(data, err);
+    });
+}
+
+class Fstream {
+    constructor(_0x01) {
+        this.nameFile = _0x01;
+    }
+    writeFile(contentFile, callback) {
+        fs.writeFile(this.nameFile, contentFile, callback);
+    }
+    openFile(param, callback) {
+        fs.open(this.nameFile, param, callback);
+    }
+    appendFile(contentFile, callback) {
+        fs.appendFile(this.nameFile, contentFile, callback);
+    }
+}
+
+exports.fstream = (nameFile) => new Fstream(nameFile);
+
+exports.appendFile = function(nameFile, contentFile, callback) {
+    fs.appendFile(nameFile, contentFile, callback);
+}
+
+exports.openFile = function(nameFile, param, callback) {
+    fs.open(nameFile, param, callback);
+}
+
+exports.writeFile = function(nameFile, contentFile, callback) {
+    fs.writeFile(nameFile, contentFile, callback);
+}
+
+let nameFunctions = [];
+let allFunctions = [];
+
+exports.functions = function(nameFuntion, callback) {
+    nameFunctions.push(nameFuntion);
+    allFunctions.push(callback);
+}
+
+exports.call = function(nameFuntion, ...param) {
+    for (let _0x01 = 0; _0x01 < nameFunctions.length; _0x01++) {
+        if (nameFunctions[_0x01] == nameFuntion) {
+            return allFunctions[_0x01](param);
+        }
+    }
+    throw new Error('Error Not Get Functions');
+    return 0;
+}
+
+exports.importFunctions = function(nameFunction) {
+    for (let _0x01 = 0; _0x01 < nameFunctions.length; _0x01++) {
+        if (nameFunctions[_0x01] == nameFunction) {
+            return allFunctions[_0x01];
+        }
+    }
+    throw new Error('Error Not Get Functions');
+    return 0;
+}
+
+exports.deleteFunctions = function(nameFill) {
+    let target;
+    for (let _0x01 = 0; _0x01 < nameFunctions.length; _0x01++) {
+        if (nameFunctions[_0x01] == nameFill) {
+            if (_0x01 == 0) {
+                nameFunctions.shift();
+                allFunctions.shift();
+                return 0;
+            } else {
+                target = _0x01;
+            }
+            break;
+        }
+    }
+    nameFunctions.splice(target, target);
+    allFunctions.splice(target, target);
+}
+
+exports.randomNumber = () => Math.random();
+exports.randomString = function(_0x02 = 0) {
+    let str = "qwertyuiopasdfghjklzxcvbnm";
+    let result = "";
+    str = str.split("");
+    if (_0x02 == 0) {
+        _0x02 = str.length;
+    }
+    for (let _0x01 = 0; _0x01 < _0x02; _0x01++) {
+        result += str[Math.floor(Math.random() * str.length)];
+    }
+    return result;
+}
+
+class enums {
+    constructor(x) {
+        this._0x01 = [];
+        this._0x02 = [0];
+        for (let _0x02 = 0; _0x02 < x.length; _0x02++) {
+            this._0x01.push(x[_0x02]);
+            this._0x02.push(_0x02 + 1);
+        }
+    }
+    changeFirstNumber(_0x01) {
+        for (let _0x03 = 0; _0x03 < this._0x01.length; _0x03++) {
+            this._0x02[_0x03] = _0x03 + _0x01;
+        }
+    }
+    getEnum(_0x01) {
+        for (let _0x02 = 0; _0x02 < this._0x01.length; _0x02++) {
+            if (this._0x01[_0x02] == _0x01) {
+                return this._0x02[_0x02];
+            }
+        }
+        return null;
+    }
+}
+
+exports.toChar = (text) => text.split("");
+exports.toArrText = (text) => text.split(" ");
+exports.int = (data) => parseInt(data);
+exports.float = (data) => parseFloat(data);
+
+exports.sort = function(_0x01) {
+    let _0x02 = _0x01.length;
+    for (let _0x03 = 0; _0x03 < _0x02; _0x03++) {
+        for (let _0x04 = 0; _0x04 < _0x02; _0x04++) {
+            if (_0x01[_0x04] > _0x01[_0x04 + 0x01]) {
+                let _0x05 = _0x01[_0x04];
+                _0x01[_0x04] = _0x01[_0x04 + 0x01];
+                _0x01[_0x04 + 0x01] = _0x05;
+            }
+        }
+    }
+    return _0x01;
+}
+
+exports.enum = function(x) {
+    return new enums(x);
+}
+
+exports.print = function(x, ...y) {
+    let Str = "";
+    Str += x;
+    for (let _0x01 = 0; _0x01 < y.length; _0x01++) {
+        Str += y[_0x01];
+    }
+    console.log(Str);
+}
+
+exports.jsostream = function(callback) {
+    try {
+        callback(process.argv.length, process.argv);
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+exports.runSave = function(functionsis) {
+    try {
+        functionsis();
+    } catch(err) {
+        return err;
+    }
+}
+
+class Variable {
+    constructor() {
+        this.varName = [];
+        this.varFill = [];
+    }
+    pushData(nameVar, fillData) {
+        this.varName.push(nameVar);
+        this.varFill.push(fillData);
+    }
+    getData(_0x01Var) {
+        for(let _0x01 = 0; _0x01 < this.varName.length; _0x01++) {
+            if(this.varName[_0x01] == _0x01Var) {
+                return this.varFill[_0x01];
+            }
+        }
+        return "_0b0wifniqfneoafboa";
+    }
+    DeleteData(nameVariable = null) {
+        nameVariable = nameVariable.trim();
+        if((nameVariable == null) || (nameVariable == "")) {
+            throw new Error('Please Give Name Of Variable');
+        }
+        let target;
+        for (let _0x01 = 0; _0x01 < this.varName.length; _0x01++) {
+            if (this.varName[_0x01] == nameVariable) {
+                if (_0x01 == 0) {
+                    this.varName.shift();
+                    this.varFill.shift();
+                    return 0;
+                } else {
+                    target = _0x01;
+                }
+                break;
+            }
+        }
+        this.varName.splice(target, target);
+        this.varFill.splice(target, target);
+    }
+}
+
+let _0x0a1Var = new Variable();
+
+exports.var = function(nameVariable = null, fillVarible) {
+    nameVariable = nameVariable.trim();
+    if((nameVariable == null) || (nameVariable == "")) {
+        throw new Error('Please Give Name Of Variable');
+    }
+    _0x0a1Var.pushData(nameVariable, fillVarible);
+}
+
+exports.vEquation = function(variableA, ...Variables) {
+    let targetA = 0;
+    for(let _0x01 = 0; _0x01 < _0x0a1Var.varName.length; _0x01++) {
+        if(_0x0a1Var.varName[_0x01] == variableA) {
+            targetA = _0x01;
+        }
+    }
+    for(let _0x02 = 0; _0x02 < Variables.length; _0x02++) {
+        for(let _0x01 = 0; _0x01 < _0x0a1Var.varName.length; _0x01++) {
+            if(Variables[_0x02] == _0x0a1Var.varName[_0x01]) {
+                _0x0a1Var.varFill[_0x01] = _0x0a1Var.varFill[targetA];
+            }
+        }    
+    }
+}
+
+exports.vGet = function(nameVariable = null) {
+    nameVariable = nameVariable.trim();
+    if((nameVariable == null) || (nameVariable == "")) {
+        throw new Error('Please Give Name Of Variable');
+    }
+    let src = _0x0a1Var.getData(nameVariable);
+    if(src == "_0b0wifniqfneoafboa") {
+        throw new Error('Name Variable Undefined');
+    } else {
+        return src;
+    }
+}
+
+exports.vDelete = function(nameVariable = null) {
+    nameVariable = nameVariable.trim();
+    if((nameVariable == null) || (nameVariable == "")) {
+        throw new Error('Please Give Name Of Variable');
+    }
+    _0x0a1Var.DeleteData(nameVariable);
+}
